a7fd8726feb646314e6a9c0933065353ca243f39
BOL CHRISTOPHE <christophe.bol@minfin.fed.be>
2021-05-05 10:23:15 +0200

    Change development backend to heroku to make e2e work with default config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // backendUrl: 'https://eurder.herokuapp.com',
-  backendUrl: 'http://localhost:8080',
+  backendUrl: 'https://eurder.herokuapp.com',
+  // backendUrl: 'http://localhost:8080',
 };
 
 /*
